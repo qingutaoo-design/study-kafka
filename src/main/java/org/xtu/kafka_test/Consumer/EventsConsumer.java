e4848c7cc3c33dfc71a91b26f2e2cb0fcb3c2ca5
QinGutaoo <qingutaoo@gmail.com>
2026-04-30 01:02:12 +0800

    消费者接收参数的一些注解使用方式

--- src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
+++ src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
@@ -1,6 +1,8 @@
 package org.xtu.kafka_test.Consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +10,14 @@ import org.springframework.stereotype.Component;
 public class EventsConsumer {
 
     @KafkaListener(topics = "hello-topic", groupId = "helloGroup")
-    public void onEvent(@Payload String Event){
+    public void onEvent(@Payload String Event,
+                        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                        @Header(KafkaHeaders.RECEIVED_KEY) String key,
+                        @Header("header1") String value1) {
         System.out.println("Received event: " + Event);
+
+        System.out.println("topic: " + topic + ", partition: " + partition + ", key: " + key + ", value1: " + value1);
 
 
     }
