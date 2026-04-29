3063e754874b0d8d9435d68dfec948548c22b984
QinGutaoo <qingutaoo@gmail.com>
2026-04-30 00:48:17 +0800

    通过注解payload标识参数是消费者消费的消息

--- src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
+++ src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
@@ -1,13 +1,14 @@
 package org.xtu.kafka_test.Consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventsConsumer {
 
-    @KafkaListener(topics = "hello-topic", groupId = "hello-group2")
-    public void onEvent(String Event){
+    @KafkaListener(topics = "hello-topic", groupId = "helloGroup")
+    public void onEvent(@Payload String Event){
         System.out.println("Received event: " + Event);
 
 
