05bf78fffb245b9618aff26321e3a08196c541ac
QinGutaoo <qingutaoo@gmail.com>
2026-05-01 17:49:01 +0800

    通过手动ack，解决消息消费失败导致的消息丢失问题

--- src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
+++ src/main/java/org/xtu/kafka_test/Consumer/EventsConsumer.java
@@ -3,6 +3,7 @@ package org.xtu.kafka_test.Consumer;
 import cn.hutool.json.JSONUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -41,7 +42,7 @@ public class EventsConsumer {
 
     }
 
-    @KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.consumer.group-id}")
+//    @KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.consumer.group-id}")
     public void onEvent3(String Event,
                          @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                          @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
@@ -53,6 +54,33 @@ public class EventsConsumer {
 
         System.out.println("ConsumerRecord: " + consumerRecord.toString());
 
+    }
+
+    /**
+     * 开启手动ack
+     * @param Event
+     * @param topic
+     * @param partition
+     * @param consumerRecord
+     * @param ack
+     */
+    //手动提交offset,默认是自动提交的，如果消费者在处理消息时发生异常，可能会导致消息丢失，因为offset已经提交了，
+    // 但是消息还没有被处理完，所以可以通过手动提交offset来解决这个问题
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "${kafka.consumer.group-id}")
+    public void onEvent4(String Event,
+                         @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                         @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                         ConsumerRecord consumerRecord,
+                         Acknowledgment ack) {
+        try {
+            User user = JSONUtil.toBean(Event, User.class);
+            System.out.println("Received event4(user): " + user);
+            System.out.println("topic: " + topic + ", partition: " + partition);
+            System.out.println("ConsumerRecord: " + consumerRecord.toString());
+            ack.acknowledge();//手动ack，提交offset
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
